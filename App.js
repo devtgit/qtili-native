0fecccb847d4c88dd7592d77cb61a33534919e91
Sergey Dedkov <dsv.mail@yandex.ru>
2023-03-22 01:56:26 +0600

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import { WebView } from 'react-native-webview'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+      <WebView
+        style={{ flex: 1, backgroundColor: '#00A300' }}
+        source={{ uri: 'http://qtili.kz' }}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        hideKeyboardAccessoryView={true}
+        originWhitelist={['*']}
+        startInLoadingState={true}
+      />
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#00A300',
   },
-});
+})
